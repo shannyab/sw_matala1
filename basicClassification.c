28832205add100f6a17cc0922370920dc775f4d3
shanny <shannya08@gmail.com>
2022-11-24 16:04:39 +0200

    fix prime functionn

--- basicClassification.c
+++ basicClassification.c
@@ -4,17 +4,17 @@
 int fac(int);
 
 int isPrime(int n) {
-    if(n ==0)
+    if(n == 0)
         return 0;
 
     int i;
 
-    for(i =2; i <sqrt(n); ++i){
+    for(i =2; i <= sqrt(n); ++i){
         if (n%i == 0)
-            return 1;
+            return 0;
     }
 
-    return 0;
+    return 1;
 }
 
 int isStrong(int n) {  
